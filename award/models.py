3d49385da48a6c6a9166772717d784065aec4142
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-11 14:26:52 +0300

    create a profile model

--- award/models.py
+++ award/models.py
@@ -27,3 +27,16 @@ class Post(models.Model):
 
     def save_post(self):
         self.save()
+        
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images', default='default.png')
+    bio = models.TextField(max_length=500, default="Coffee and Code")
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+        

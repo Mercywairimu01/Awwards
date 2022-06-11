ff613b20c85d6a94a4f4ed16710b4b812934e49d
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-11 15:29:44 +0300

    add views for user registration

--- award/views.py
+++ award/views.py
@@ -1,6 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 from .models import Post,Profile
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView,DetailView,CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
@@ -13,3 +19,15 @@ def profile(request):
     
     return render(request, 'profile.html',{'set':set})
 
+def register(request):
+    if request.method == 'POST':
+        form =UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username =form.cleaned_data.get('username')
+            messages.success(request,f'Account created.Please login!')
+            return redirect('login')
+    else:
+        form =UserRegisterForm()    
+    
+    return render(request,'users/register.html',{'form':form })

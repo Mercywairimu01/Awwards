a4c6dce8c085597cb481186c11d02853375a4046
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-11 14:28:25 +0300

    add views and urls to the profile model

--- award/views.py
+++ award/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Profile
 from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     
     return render(request,'index.html', {'posts': posts})
+
+def profile(request):
+    set = Profile.objects.all()
+    
+    return render(request, 'profile.html',{'set':set})
+

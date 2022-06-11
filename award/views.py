2d7231ecc185db20d1dcea29fa43dd029c72d407
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-06-11 14:03:13 +0300

    modify the UI

--- award/views.py
+++ award/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Post
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    posts = Post.objects.all()
+    
+    return render(request,'index.html', {'posts': posts})
